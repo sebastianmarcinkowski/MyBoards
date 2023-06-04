8b12f375a9fa223dd985693952e5b664c55422d7
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-06-04 15:13:49 +0200

    Realizacja zadań do lekcji 11 - stworzenie klas, klasy DbContext oraz kluczy głównych.

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -1,7 +1,7 @@
-﻿namespace MyBoards.Entities
-{
+﻿{
     public class User
     {
+        public Guid ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
